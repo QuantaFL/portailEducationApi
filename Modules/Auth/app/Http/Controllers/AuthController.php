dfd7727bf745b88e4d5c18be69c948cd00cdd717
RBen19 <bonmerciel@gmail.com>
2025-07-10 21:35:38 +0000

    refactor(auth): update AuthController with role-based logic

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -12,45 +12,48 @@ class AuthController extends Controller
      */
     public function index()
     {
-        return view('auth::index');
-    }
+        //
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('auth::create');
+        return response()->json([]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+        //
+
+        return response()->json([]);
+    }
 
     /**
      * Show the specified resource.
      */
     public function show($id)
     {
-        return view('auth::show');
-    }
+        //
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('auth::edit');
+        return response()->json([]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id) {}
+    public function update(Request $request, $id)
+    {
+        //
+
+        return response()->json([]);
+    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id) {}
+    public function destroy($id)
+    {
+        //
+
+        return response()->json([]);
+    }
 }
