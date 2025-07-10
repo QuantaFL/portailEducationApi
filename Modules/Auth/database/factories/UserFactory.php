70f35ee552ee841947f29e8e45347ccf1007c733
RBen19 <bonmerciel@gmail.com>
2025-07-10 20:50:48 +0000

    feat(auth): create factory for user

---
+++ Modules/Auth/database/factories/UserFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Auth\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Auth\Models\Role;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = \Modules\Auth\Models\User::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'nom_utilisateur' => $this->faker->unique()->userName,
+            'mot_de_passe' => bcrypt('passer'),
+            'email' => $this->faker->unique()->safeEmail,
+            'telephone' => $this->faker->unique()->phoneNumber,
+            'role_id' => Role::pluck('id')->random(),
+        ];
+    }
+}
+
