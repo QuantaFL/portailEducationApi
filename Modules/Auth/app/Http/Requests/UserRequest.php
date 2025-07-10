477de5622d5db7167b6a66ec01eb71236a1b7c2d
RBen19 <bonmerciel@gmail.com>
2025-07-10 20:21:59 +0000

    feat(auth): create UserRequest

---
+++ Modules/Auth/app/Http/Requests/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\Auth\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'nom_utilisateur' => 'required|string|unique:users,nom_utilisateur',
+            'mot_de_passe' => 'required|string|min:8',
+            'email' => 'nullable|email|unique:users,email',
+            'role_id' => 'required|exists:roles,id',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function messages(): array
+    {
+        return [
+            'nom_utilisateur.required' => 'Le nom d’utilisateur est obligatoire.',
+            'nom_utilisateur.unique' => 'Ce nom d’utilisateur est déjà utilisé.',
+            'mot_de_passe.required' => 'Le mot de passe est obligatoire.',
+            'mot_de_passe.min' => 'Le mot de passe doit contenir au moins 8 caractères.',
+            'email.email' => 'Le champ email doit être une adresse email valide.',
+            'email.unique' => 'Cet email est déjà utilisé.',
+            'role_id.required' => 'Le rôle est obligatoire.',
+            'role_id.exists' => 'Le rôle sélectionné est invalide.',
+        ];
+    }
+}
