9df5892871af1d0ae6aab1dc9720af40a8c75bd4
RBen19 <bonmerciel@gmail.com>
2025-07-11 01:29:57 +0000

    Secure auth routes with custom JWT middleware and use AuthService

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -6,3 +6,11 @@ use Modules\Auth\Http\Controllers\AuthController;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('auths', AuthController::class)->names('auth');
 });
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
