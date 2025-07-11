0b625a694fe0076e3f80af4ad2ab48e14fc7c2f3
RBen19 <bonmerciel@gmail.com>
2025-07-11 01:29:58 +0000

    Load JWT config and necessary bindings in bootstrap file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Modules\Auth\Http\Middleware\AuthenticateMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->alias([
+            'jwt.auth'=>AuthenticateMiddleware::class
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
