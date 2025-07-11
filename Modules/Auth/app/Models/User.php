0d10b0b1f4b219b7731881f5a9fcf9057cd0cda7
RBen19 <bonmerciel@gmail.com>
2025-07-11 01:29:56 +0000

    Extend User model with JWT support and necessary fields

--- Modules/Auth/app/Models/User.php
+++ Modules/Auth/app/Models/User.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace Modules\Auth\Models;
-
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Auth\Database\Factories\UserFactory;
-class User extends Model
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
@@ -18,5 +20,23 @@ class User extends Model
     protected static function newFactory()
     {
         return \Modules\Auth\Database\Factories\UserFactory::new();
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'nom_utilisateur' => $this->nom_utilisateur,
+            'prenom'          => $this->prenom,
+            'nom'             => $this->nom,
+            'email'           => $this->email,
+            'telephone'       => $this->telephone,
+            'role_id'         => $this->role_id,
+            'statut'          => $this->statut,
+        ];
     }
 }
