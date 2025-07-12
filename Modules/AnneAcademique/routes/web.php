54a0a017da445ff5f7c31f8116627c5cfb6661d9
RBen19 <bonmerciel@gmail.com>
2025-07-12 17:30:05 +0000

    chore(routes): supprimer le controller non API dans les routes web

--- Modules/AnneAcademique/routes/web.php
+++ Modules/AnneAcademique/routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\AnneAcademique\Http\Controllers\AnneAcademiqueController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('anneacademiques', AnneAcademiqueController::class)->names('anneacademique');
-});
+
