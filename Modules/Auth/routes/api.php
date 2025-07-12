c0229ba4dab523b6793a1cdd7de5a9da2db277ae
RBen19 <bonmerciel@gmail.com>
2025-07-12 15:26:25 +0000

    chore:remove sanctum

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('auths', AuthController::class)->names('auth');
-});
+
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
