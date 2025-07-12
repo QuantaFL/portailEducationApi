bee4a21b47d57a116e25e0ec82e1032a03aca1bc
RBen19 <bonmerciel@gmail.com>
2025-07-12 17:30:04 +0000

    feat(AnneAcademique): ajouter la route apiResource pour AnneAcademique

--- Modules/AnneAcademique/routes/api.php
+++ Modules/AnneAcademique/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\AnneAcademique\Http\Controllers\AnneAcademiqueController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('anneacademiques', AnneAcademiqueController::class)->names('anneacademique');
+Route::prefix('v1')->group(function () {
+    Route::apiResource('anneacademiques', AnneAcademiqueController::class);
 });
