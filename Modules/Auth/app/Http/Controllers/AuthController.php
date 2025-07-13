2ef2a27b51128a75990563ae1cba6367ce1c7b60
RBen19 <bonmerciel@gmail.com>
2025-07-13 16:28:40 +0000

    refactor: update AuthController to use English field names

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
     }
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'mot_de_passe');
+        $credentials = $request->only('email', 'password');
 
         $result = (new \Modules\Auth\Services\AuthService)->login($credentials);
 
