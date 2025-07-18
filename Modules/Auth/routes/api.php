873ea1c7dfa9ddab0d6387f323bfcc8a9f475ac6
RBen19 <bonmerciel@gmail.com>
2025-07-18 17:02:45 +0000

    chore(auth): register changePassword route in API routes

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -7,6 +7,7 @@ use Modules\Auth\Http\Controllers\AuthController;
 Route::prefix('v1/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/changePassword',[AuthController::class,'changePassword']);
 });
 Route::middleware('jwt.auth')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
